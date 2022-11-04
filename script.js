65986330bba2b0b5c2a8d38152b69ffa93fb58a4
afinn29 <115803872+finnland29@users.noreply.github.com>
2022-11-04 15:18:15 +0000

    Remote save initial work

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// example function call to open a dialog box
+// examples function call to display a dialog box
 // let's comment out the code below, which is top-level code 
 // that automatically runs when the script is loaded
 // alert("welcome, this is a javascript popup alert");
@@ -6,20 +6,32 @@
 function submitForm(name, hometown) {
   // search the DOM for a specific tag with the id "guestlist"
   var guestlist = document.getElementById("guestlist");
-  var n = document.getElementById("guestlist");
+  var n = document.getElementById("name");
   var h = document.getElementById("hometown");
-var currentguestlist = localStorage.getItem("answerlist");
-  localStorage.setItem("guestlist", currentguestlist + "<li>" + name + ". " + hometown + "</li>");
-  guestlist.innerHTML += "<li>" + name + ". " + hometown + "</li>";
+  //saveLocal(name,hometown);
+  saveRemote(name,hometown);
+  guestlist.innerHTML += "<li>" = name + ". " + hometown + "</li>";
   n.value = "";
   h.value = "";
   n.focus();
   return false;
 }
 
+function saveLocal() {
+  var currentguestlist = localStorage.getItem("guestlist");
+  localStorage.setItem("guestlist", currentguestlist + "<li>" + name + ". " + hometown + "</li>");
+}
+
+function saveRemote(name, hometown) {
+  $.get("save.php",{"name":name,"hometown":hometown});
+}
+
+
+
 function clearStorageGB() {
-  var guestlist = document.getElementById("guestlist");
-  guestlistlist.innerHTML = "";
+  //var guestlist = document.getElementById("guestlist");
+  //guestlistlist.innerHTML = "";
+  $("#guestlist").html("The guest list has been cleared.");
   localStorage.setItem("guestlist","");
 }
 
